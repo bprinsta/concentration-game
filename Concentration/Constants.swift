9191e124fdd44d2a7b2b37c24325fa61a7d63e70
Ben Musoke-Lubega <benmusokelubega@princeton.edu>
2019-12-18 12:48:45 -0500

    added info button

--- Concentration/Constants.swift
+++ Concentration/Constants.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 struct Constants {
+	// Main Theme Colors
 	static let lightPink = UIColor(rgb: 0xEDC7B7)
 	static let navyBlue = UIColor(rgb: 0x123C69)
 	static let beige = UIColor(rgb: 0xEEE2DC)
 	
+	// Brown Theme Colors
 	static let brown = UIColor(rgb: 0xbe9b7b)
 	static let darkCream = UIColor(rgb: 0xffdeb3)
 	static let cream = UIColor(rgb: 0xfff4e6)
 	
+	// Green Theme Colors
 	static let green = UIColor(rgb: 0xc8d5b9)
 	static let forestCream = UIColor(rgb: 0xFAF3DD)
 	static let darkGreen = UIColor(rgb: 0x4A7C59)
 	
+	// Blue & Brown Theme Colors
 	static let beachBlue = UIColor(rgb: 0xb3d6ff)
 	static let lightBeachBlue = UIColor(rgb: 0xe3f0ff)
 	static let beachCream = UIColor(rgb: 0x663d00)
 	
+	// Emojis
 	static let faces = ["😎", "🤪", "😁", "😍", "🤑", "😷", "😫", "🤩", "😅", "🤐", "😛", "😬"]
 	static let animals = ["🐶", "🐱", "🐭", "🐷", "🐮", "🐨", "🙉", "🦊", "🐻", "🐸", "🐙", "🐼"]
 	static let fruit = ["🍎", "🍐", "🌶", "🥦", "🍆", "🥑", "🍌", "🍇", "🍑", "🌽", "🍉", "🍒"]
